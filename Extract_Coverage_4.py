2065965be67e8498d37eb44e89459ead8faea087
AbbasDehnad <83868773+AbbasDehnad@users.noreply.github.com>
2022-07-30 17:18:55 +0200

    solve genotype issue

--- Extract_Coverage_4.py
+++ Extract_Coverage_4.py
@@ -1,6 +1,7 @@
 #  pip install scikit-allel
 
 import csv
+from random import sample
 import pandas as pd
 import allel
 import glob, os
@@ -11,9 +12,14 @@ vcf_files = glob.glob("*.vcf")
 for file in vcf_files:
     # read snp chrom location from file
     df = allel.vcf_to_dataframe(file, fields="*")
+    # print(
+    #     df.columns,
+    #     df.loc[0],
 
+    # )
     callset = allel.read_vcf(file, fields="*", alt_number=3)
     cromosoms = sorted(list(set(callset["variants/CHROM"])))
+    # print(sorted(callset.keys()),allel.GenotypeArray(callset['calldata/GT']))
     dict_of_chromosoms = {}
     for index, chr in enumerate(cromosoms):
         dict_of_chromosoms[chr] = (
@@ -47,17 +53,19 @@ for file in vcf_files:
         writer = csv.writer(f)
         writer.writerow(["snp", "chromosome", "position"])
         # sort based on pos
-        for row in list(snp_out).sort(key=lambda x: x[2]):
+        for row in sorted(list(snp_out), key=lambda x: x[2]):
             writer.writerow(row)
 
     with open(file + "geno_loc.csv", "w") as f:
         writer = csv.writer(f)
         writer.writerow(["chr", "position", "GT"])
-        geno_out =zip(
+        geno_out = zip(
             [dict_of_chromosoms.get(chr) for chr in callset["variants/CHROM"]],
             callset["variants/POS"],
-            [1 if str(gt) == "[[ 1 -1]]" else 0 for gt in callset["calldata/GT"]],
+            [
+                1 if gt[0][0] == 1 else 0
+                for gt in allel.GenotypeArray(callset["calldata/GT"])
+            ],
         )
-        for row in geno_out :  # TODO check GT is correct
-
+        for row in geno_out:  # TODO check GT is correct
             writer.writerow(row)
