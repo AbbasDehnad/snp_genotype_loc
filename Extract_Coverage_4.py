d1a153f8219c074dd90f9d6a83164bfc66f00a55
AbbasDehnad <83868773+AbbasDehnad@users.noreply.github.com>
2022-07-30 23:49:07 +0200

    create 2d table-rename snps

--- Extract_Coverage_4.py
+++ Extract_Coverage_4.py
@@ -5,10 +5,14 @@ from random import sample
 import allel
 import glob
 
+from pandas import DataFrame
+
 vcf_files = glob.glob("*.vcf")
 # read positionsFile
 all_loc = []
 all_geno = []
+dict_chr_pos_to_snp = {}
+num_of_snps = 0
 chromosoms = set()
 # read chromosoms from vcf files
 for file in vcf_files:
@@ -21,46 +25,65 @@ for index, chr in enumerate(sorted(list(chromosoms))):
     dict_of_chromosoms[chr] = "chr" + "{0:02}".format(index + 1)
 
 print(dict_of_chromosoms)
-
+file_num = 1
 for file in vcf_files:
+    print("file:", file)
     # read snp chrom location from file
     df = allel.vcf_to_dataframe(file, fields="*")
     callset = allel.read_vcf(file, fields="*", alt_number=3)
-
+    GT = callset["calldata/GT"]
     unique_set: set[str, str] = set()
     snp_out = []
     geno_out = []
+    geno_out_1 = []
     for index, chrom_row in df.iterrows():
+        num_of_snps += 1
         cromosom = dict_of_chromosoms.get(chrom_row["CHROM"])
-
+        if dict_chr_pos_to_snp.get((cromosom, chrom_row["POS"])):
+            snp = dict_chr_pos_to_snp.get((cromosom, chrom_row["POS"]))
+        else:
+            snp = "snp" + str(num_of_snps)
+            dict_chr_pos_to_snp[cromosom, chrom_row["POS"]] = snp
         # add snps but dont add duplicates
-        if chrom_row["is_snp"] and (cromosom, chrom_row["POS"]) not in unique_set:
-            snp = "snp" + "{0:03}".format(index + 1)
-            snp_out.append((snp, cromosom, chrom_row["POS"], file))
+        if (cromosom, chrom_row["POS"]) not in unique_set:
+
+            geno_out_1.append((snp, 1 if GT[index][0][0] == 1 else 0, file_num))
+            snp_out.append((snp, cromosom, chrom_row["POS"]))
         unique_set.add((cromosom, chrom_row["POS"]))
-        geno_out = zip(
-            [dict_of_chromosoms.get(chr) for chr in callset["variants/CHROM"]],
-            callset["variants/POS"],
-            [
-                1 if gt[0][0] == 1 else 0
-                for gt in allel.GenotypeArray(callset["calldata/GT"])
-            ],
-            [file for _ in callset["calldata/GT"]],
-        )
+
+    geno_out = zip(
+        [dict_of_chromosoms.get(chr) for chr in callset["variants/CHROM"]],
+        callset["variants/POS"],
+        [
+            1 if gt[0][0] == 1 else 0
+            for gt in allel.GenotypeArray(callset["calldata/GT"])
+        ],
+        [file_num for _ in callset["calldata/GT"]],
+    )
     all_loc.extend(sorted(list(snp_out), key=lambda x: (x[1], x[2])))
-    all_geno.extend(sorted(list(geno_out), key=lambda x: (x[0], x[1])))
+    # all_geno.extend(sorted(list(geno_out), key=lambda x: (x[0], x[1])))
+    all_geno.extend(sorted(list(geno_out_1)))
+    file_num += 1
+
+# first convert it to 2d table
+list_to_convert = [[0] * (len(vcf_files) + 1) for _ in range(num_of_snps + 1)]
+for row in all_geno:
+    snp = row[0]
+    col_index = row[2]
+    row_index = int(snp[3:]) - 1
+    list_to_convert[row_index][0] = snp
+    list_to_convert[row_index][col_index] = row[1]
 
 
 with open("snp_loc.csv", "w", newline="") as f:
     writer = csv.writer(f)
-    writer.writerow(["snp", "Chromosome", "position", "file_name"])
+    writer.writerow(["snp", "Chromosome", "position"])
     # sort based on pos
     for row in all_loc:
         writer.writerow(row)
 
 with open("geno_loc.csv", "w", newline="") as f:
     writer = csv.writer(f)
-    writer.writerow(["Chromosome", "position", "GT", "file_name"])
-
-    for row in all_geno:  # TODO check GT is correct
+    writer.writerow(["SNP", *vcf_files])
+    for row in list_to_convert:  # TODO check GT is correct
         writer.writerow(row)
